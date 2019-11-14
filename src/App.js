965dfba275473c4e4ef0a86f77e2a578fe681cd3
XX <fallwall19@gmail.com>
2019-11-14 15:34:25 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <p>portfolio</p>
+      </div>
+    );
+  }
+}
+
+export default App;
